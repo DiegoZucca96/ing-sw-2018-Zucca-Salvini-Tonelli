c9eaf31e3531611cb7b4ca9889a6ef63a8bb2982
ilTone <alessio1.tonelli@mail.polimi.it>
2018-05-11 09:29:00 +0200

    StartGame recalled GUI

--- src/main/java/ingsw/Client.java
+++ src/main/java/ingsw/Client.java
@@ -1,10 +1,10 @@
 package ingsw;
 
-import ingsw.view.StartGame;
+import ingsw.view.GUI;
 
 public class Client {
     public static void main(String[] args) throws Exception {
         //In teoria qua si deve passare al metodo grafico il controller per far eseguire le varie operazioni
-        new StartGame().main();
+        new GUI().main();
     }
 }
