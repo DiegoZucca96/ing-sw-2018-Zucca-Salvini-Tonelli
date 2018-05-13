f9f7c7317d11525f20c40b0a6dedfd4f88261c4f
ilTone <alessio1.tonelli@mail.polimi.it>
2018-05-13 11:30:41 +0200

    Impossible to close your PV

--- src/main/java/ingsw/view/Private.java
+++ src/main/java/ingsw/view/Private.java
@@ -1,12 +1,15 @@
 package ingsw.view;
 
 import ingsw.model.InitializerView;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 
 //ASSEGNAZIONE CARTA PRIVATA
 
@@ -28,6 +31,10 @@ public class Private {
         selectedImage.setImage(imagepv);
 
         privateObj.resizableProperty().setValue(Boolean.FALSE);
+
+        Platform.setImplicitExit(false);
+
+        privateObj.setOnCloseRequest(event -> event.consume());
 
         p.getChildren().add(selectedImage);
         privateObj.setTitle(" Your Private Card");
