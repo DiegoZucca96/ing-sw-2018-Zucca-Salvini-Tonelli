3791d90a6877a18e227897ff46eba4d074e2e02f
2B9S <10490058@polimi.it>
2018-05-16 12:04:26 +0200

    Nuovo metodo getRandomWPs in Controller

--- src/main/java/ingsw/controller/Controller.java
+++ src/main/java/ingsw/controller/Controller.java
@@ -3,6 +3,9 @@ package ingsw.controller;
 import ingsw.Server;
 import ingsw.model.InitializerView;
 import ingsw.model.Match;
+import ingsw.model.RandomGenerator;
+import ingsw.model.WindowPFactory;
+import ingsw.view.WindowPattern;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -10,11 +13,16 @@ import java.util.ArrayList;
 
 public class Controller extends UnicastRemoteObject implements RMIController {
 
-    Server server;
+    private Server server;
+    private WindowPFactory wpFactory;
+    private RandomGenerator rg;
+
 
     public Controller(Server server) throws RemoteException {
         super();
         this.server= server;
+        wpFactory = new WindowPFactory();
+        rg = new RandomGenerator(wpFactory.getNumOfWPs());
     }
 
     //Lista dei vari metodi invocabili da grafica che vanno a interagire con il model
@@ -27,13 +35,17 @@ public class Controller extends UnicastRemoteObject implements RMIController {
     public void addPlayers(String account){
         server.addPlayers(account);
     }
+
+    //aggiunge il nuovo account alla lista degli account del server, se l'account è già presente restituisce false
     @Override
-    public void addAccount(String account){
+    public synchronized boolean addAccount(String account){
+        if (access(account)) return false;
         server.addAccount(account);
+        return true;
     }
 
     @Override
-    public boolean access(String account){
+    private boolean access(String account){
         //se esiste già il nome salvato nel server non puoi accedere
         if(server.getListOfClient().contains(account)){
             return true;
@@ -41,6 +53,14 @@ public class Controller extends UnicastRemoteObject implements RMIController {
         return false;
     }
 
+    public synchronized ArrayList<WindowPattern> getRandomWPs(){
+        ArrayList<WindowPattern> wps = new ArrayList<>();
+        wps.add(wpFactory.createWindowPattern(rg.random()));
+        wps.add(wpFactory.createWindowPattern(rg.random()));
+        wps.add(wpFactory.createWindowPattern(rg.random()));
+        wps.add(wpFactory.createWindowPattern(rg.random()));
+        return wps;
+    }
 
     //Chiama il metodo inizializzatore del Match
     @Override
