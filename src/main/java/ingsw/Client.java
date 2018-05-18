9862119d382b3d0cb1e02343ce2794681b44ccf9
2B9S <37152422+2B9S@users.noreply.github.com>
2018-05-18 16:09:57 +0200

    Supporto per i socket lato client

--- src/main/java/ingsw/Client.java
+++ src/main/java/ingsw/Client.java
@@ -2,11 +2,54 @@ package ingsw;
 
 import ingsw.view.GUI;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Client {
 
+    private String ip;
+    private int port;
+    private Scanner in;
+    private PrintWriter out;
+    private Socket socket;
+
+    public Client(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
     public static void main(String[] args) throws Exception {
+        Client client = new Client("127.0.0.1",1080);
         //In teoria qua si deve passare al metodo grafico il controller per far eseguire le varie operazioni
         new GUI().main();
+        try{
+            client.startClientSocket();
+        }catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void startClientSocket() throws IOException {
+        try{
+            setup();
+            //...
+        }catch(NoSuchElementException e){
+            System.err.println(e.getMessage());
+        }
+        finally{
+            in.close();
+            out.close();
+            socket.close();
+        }
+    }
+
+    private void setup() throws IOException {
+        socket = new Socket(ip, port);
+        in = new Scanner(socket.getInputStream());
+        out = new PrintWriter(socket.getOutputStream());
     }
 
 }
--- src/main/java/ingsw/ServerHandler.java
+++ src/main/java/ingsw/ServerHandler.java
@@ -1,11 +1,15 @@
 package ingsw;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ServerHandler implements Runnable {
 
     private Socket socket;
+    private Scanner in;
+    private PrintWriter out;
 
     public ServerHandler(Socket socket){
         this.socket=socket;
@@ -13,10 +17,17 @@ public class ServerHandler implements Runnable {
 
     public void run(){
         try {
+            setup();
+            //...
             socket.close();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    private void setup() throws IOException {
+        in = new Scanner(socket.getInputStream());
+        out = new PrintWriter(socket.getOutputStream(), true);
     }
 
 }
