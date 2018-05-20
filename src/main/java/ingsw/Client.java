d6ad6f7488cec142e880d34d3175ca3d0987ff54
2B9S <37152422+2B9S@users.noreply.github.com>
2018-05-20 19:49:06 +0200

    Nuovi metodi in Client

--- src/main/java/ingsw/Client.java
+++ src/main/java/ingsw/Client.java
@@ -1,8 +1,22 @@
 package ingsw;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public interface Client {
 
-     void startClient() throws IOException;
+    //avvia il client,fa setup della connessione
+    void startClient() throws IOException;
+    /*
+    //permette al gicatore di fare login, restituisce false se l'utente non è registrato
+    boolean login(String nickname);
+
+    //permette al giocatore di registrarsi, restituisce false se il nickname è già usato
+    boolean register(String nickname);
+
+    //restituisce un array contenente i dati per renderizzare le 4 wp
+    ArrayList<Integer>[][] getRandomWps();
+*/
+
+
 }
