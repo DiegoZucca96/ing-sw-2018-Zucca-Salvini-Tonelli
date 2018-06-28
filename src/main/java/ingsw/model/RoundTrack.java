6896c21e4d0fba83189cfd42dfdb7f27a1ad7d95
2B9S <37152422+2B9S@users.noreply.github.com>
2018-06-28 20:08:14 +0200

    javaDoc RoundTrack

--- src/main/java/ingsw/model/RoundTrack.java
+++ src/main/java/ingsw/model/RoundTrack.java
@@ -4,13 +4,15 @@ import ingsw.observers.Observer;
 import ingsw.observers.RoundTrackObserver;
 import java.util.ArrayList;
 
-//Classe che rappresenta la round track del gioco, contiene tutti i dadi che avanzano nella draft pool alla fine di ogni round,
-//memorizza il round corrente.
 
+/**
+ * Class that represents the round track of the game. It contains the dice in excess at the end of each round,
+ * it memorizes the current round
+ */
 public class RoundTrack {
 
     private int currentRound;
-    private ArrayList<ArrayList<Die>> extraDice;
+    private ArrayList<ArrayList<Die>> extraDice;    //dice in excess at the end of ech round
     private Observer viewObserver;
 
     public RoundTrack() {
@@ -19,23 +21,32 @@ public class RoundTrack {
         viewObserver = new RoundTrackObserver();
     }
 
-    //restituisce il round corrente
+    //simple getter
     public int getRound() {
         return currentRound;
     }
 
-    //imposta il round corrente
+    /**
+     * setter that verifies the value of the new round is between 1 and 10
+     * @param currentRound
+     */
     public void setRound(int currentRound) {
         if(currentRound >= 1 && currentRound <=10) this.currentRound = currentRound;
     }
 
-    //incrementa il round corrente di uno, fino a raggiungere il valore massimo, 10
+    /**
+     * This method increases of one the value of the current round until it reaches 10
+     */
     public void nextRound(){
         if(currentRound <10)
             currentRound++;
     }
 
-    //inserisce il dado nella lista all'indice round.
+    /**
+     * It adds a new die in the round track
+     * @param die   die added in the round track
+     * @param round round in which you want to place the die
+     */
     public void addDie(Die die, int round){
         if(round>=1 && round<=10){
             for(int i=extraDice.size(); i<round; i++){
@@ -46,7 +57,12 @@ public class RoundTrack {
         }
     }
 
-    //restituisce il riferimento al dado nella posizione index della lista nella poszione round
+    /**
+     * getter
+     * @param round round of the requested die
+     * @param index position (sequential number) of the requested die in the round specified
+     * @return die that belong to the round requested. in position index
+     */
     public Die getDie(int round, int index){
         if(round<1 || round>10) return null;
         try{
@@ -56,6 +72,13 @@ public class RoundTrack {
         }
     }
 
+    /**
+     * ???
+     * @param round
+     * @param index
+     * @param dpDie
+     * @return
+     */
     //esegue getDie e rimuove il riferiemento al dado dalla lista.
     public Die replaceDie(int round, int index, Die dpDie){
         Die result = getDie(round, index);
@@ -65,10 +88,17 @@ public class RoundTrack {
         return result;
     }
 
+    /**
+     * This method notifies changes in this object to viewObserver
+     */
     public void notifyViewObserver(){
         viewObserver.update(this, ViewData.instance());
     }
 
+    /**
+     * This method creates a representation of this class
+     * @return  list of strings that represents this object
+     */
     public ArrayList<String> toArrayString() {
         ArrayList<String> newRoundTrack = new ArrayList<>();
         for(int i=0; i<10; i++){
