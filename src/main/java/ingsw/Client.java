b3afaf3e22d11afb262dd8e76f020af5d957ed64
2B9S <10490058@polimi.it>
2018-07-02 16:40:31 +0200

    javadoc client interface

--- src/main/java/ingsw/Client.java
+++ src/main/java/ingsw/Client.java
@@ -12,147 +12,323 @@ import java.util.ArrayList;
 
 public interface Client {
 
-    //avvia il client,fa setup della connessione
+    /**
+     * This method begins connection with server
+     * @param hostAddress   server address
+     * @throws IOException  exception due to connection error
+     */
     void startClient(String hostAddress) throws IOException;
 
-    //permette al gicatore di fare login, restituisce false se l'utente non è registrato
+    /**
+     * This method allows client to log in
+     * @param nickname player's nickname (if he is registered)
+     * @return false if player can't log in
+     */
     boolean login(String nickname);
 
-    //permette al giocatore di registrarsi, restituisce false se il nickname è già usato
+    /**
+     * This method allows client to register
+     * @param nickname  player's nickname
+     * @return  false if nickname is already used
+     */
     boolean register(String nickname);
 
-    //Metodo che crea le 4 WP da scegliere ritornando le informazioni per creare il background dei bottoni
+    /**
+     * This method allows player to see 4 window patterns among which the player has to choose his one
+     * @return 4 window patterns to render view side
+     */
     ArrayList<ViewWP> getRandomWps();
 
+    /**
+     * ???
+     * @param wp
+     */
     void addWP(ViewWP wp);
 
-    //Restituisce la lista dei nomi dei giocatori
+    /**
+     *
+     * @return player's nickname list
+     */
     ArrayList<String> getListOfPlayers();
 
+    /**
+     * ???
+     * @return
+     */
     //Ritornano le coordinate cliccate dal giocatore
     int getCoordinateSelectedY();
 
-    //prende dalla draftpool il dado in posizione (row, column)
+    /**
+     * This method allows player to take a die from the draft pool
+     * @param row   ???
+     * @param col   ???
+     * @return  false if player can't take the requested die
+     */
     boolean takeDie(int row, int col);
 
-    //prende un dado in posizione (row, column) dalla wp del giocatore corrente,
-    //restituisce false se il dado non può essere preso
+    /**
+     * This method allows player to take a die from his window pattern
+     * @param row   row in which the requested die is placed
+     * @param column column in which requested die is placed
+     * @return  false if die can't be taken
+     */
     boolean takeWPDie(int row, int column);
 
-    //posiziona il dado nelle coordinate row, column della wp del giocatore
-    //restitusce false se si violano le restrizioni
+    /**
+     * This method allows player to place the previously taken die in his window pattern
+     * @param row   row in which the requested die has to be placed
+     * @param column column in which the requested die has to be placed
+     * @return false if die can't be placed
+     */
     boolean positionDie(int row, int column);
 
-    //passa il turno
+    /**
+     * This method allow player to skip, or end, his turn
+     */
     void skip();
 
-    //utilizza la tool card, parameter è una stringa che serve per creare l'oggetto di tipo PlayerUseTool
-    //restituisce false se la carta non può essere usata
+    /**
+     * This method allows player to use a tool card
+     * @param i tool card number
+     * @param parameter player's parameters to use the tool card
+     * @return  false if the tool card can't be used
+     */
     boolean useToolCard(int i, ToolView parameter);
 
-    //restituisce lo stato del giocatore, "enabled" -> attivo. "disabled" -> disattivo (non è il suo turno)
+    /**
+     *
+     * @return  player's state (during his turn the state is "enabled", if he's waiting for his turn the state
+     *          is "disabled")
+     */
     String getPlayerState();
 
-    //Restituisce il numero dei giocatori della partita
+    /**
+     *
+     * @return number of match players
+     */
     int getNumberOfPlayers();
 
-    //restituisce arraylist della scelta di tutti i giocatori
+    /**
+     *
+     * @param name  ???
+     * @return object used to render the windows pattern chosen by players
+     */
     ArrayList<ViewWP> getPlayerWPs(String name);
 
-    //mette il giocatore in attesa di nuovi giocatori
+    /**
+     * This method put player on hold of others players
+     * @return false until match starts
+     */
     boolean waitForPlayers();
 
-    //restituisce una lista di stringhe rappresentanti gli oggetti da inizializzare nella view
+    /**
+     *
+     * @return an object containing all necessary data to render view GUI side
+     */
     ViewData initializeView();
 
+    /**
+     *
+     * @return an object containing all necessary data to render view CLI side
+     */
     ViewData initializeViewCLI();
 
-    //restituisce una lista di stringhe rappresentanti gli oggetti da aggiornare nella view
+    /**
+     *
+     * @return an object containing all necessary data to update view GUI and CLI side
+     */
     ViewData updateView();
 
-    //restituisce true se tutti hanno scelto la propria wp e si avvia la partita
+    /**
+     *
+     * @return true if the match can be started (after that players has chosen their windows pattern)
+     */
     boolean readyToPlay();
 
-    //restituisce il nome del client che viene chiamato
+    /**
+     *
+     * @return client's name
+     */
     String getName();
 
+    /**
+     *  ???
+     * @param nameWindow
+     * @param nameClient
+     */
     //crea un hash di valori player-WP scelta (credo aggiunga la wp scelta all'hashmap del controller)
     void createHash(int nameWindow, String nameClient);
 
+    /**
+     *
+     * @param name player's name
+     * @return  player's private objective card
+     */
     String getPVCard(String name);
 
+    /**
+     * ???
+     * @param active
+     */
     //serve per controllare che il client sia attivo
     void setActive(Boolean active);
 
-    //attivo o no?
+    /**
+     * this method allows client to understand if server considers him inactive due to the fact the timer finished
+     * and players hasn't made any move during his turn
+     * @return true if server considers him active
+     */
     boolean getActive();
 
-    //tempo per fare una mossa
+    /**
+     *
+     * @return seconds that player has to make a move
+     */
     int getTimeMove();
 
-    //il turno di?
+    /**
+     *
+     * @return player who has the right to play in the moment of the call of this method
+     */
     String getCurrentPlayer();
 
+    /**
+     * ???
+     */
     //Setta i valori di selection player a null in caso di annullamento della mossa
     void nullSelection();
 
-    //Restituisce il round
+    /**
+     *
+     * @return current match round
+     */
     int getRound();
 
-    //Dice se il match è già stato trovato o meno
+    /**
+     *
+     * @return true if match is already been found
+     */
     boolean matchFound();
 
-    //Restituisce la WP che era scelta dal player
+    /**
+     * This method allows client to render player's window pattern
+     * @param userName player's nickname
+     * @return  an object containing all necessary data to render player's window pattern
+     */
     ViewWP getWP(String userName);
 
+    /**
+     *  ???
+     */
     //Ordina le windowChosen
     void orderWPChoise();    //si scrive Choice...
 
+    /**
+     *
+     * @return true if match is finished
+     */
     //Va a controllare se la partita è giunta al termine
     boolean isFinish();
 
-    //Restituisce il punteggio del giocatore
+    /**
+     * This method allows player to know his (and others players) 's score
+     * @param name player's nickname
+     * @return player's score
+     */
     int getScore(String name);
 
-    //Avvia il calcolo dei punteggi giocatore
+    /**
+     * This method launches the calculation of players scores model side
+     */
     void calculateScore();
 
-    //Trova il vincitore
+    /**
+     *
+     * @return match winner
+     */
     String findWinner();
 
+    /**
+     * ???
+     * @return
+     */
     //Restituisce i nomi dei players (serve nella schermata finale di vittoria)
     ArrayList<String> getListOfMatchPlayers();
 
+    /**
+     * ???
+     * @return
+     */
     //i tre metodi sotto tengono conto del fatto che il giocatore abbia posizionato un dado o meno e se ha usato la tool8(per saltare il turno)
     boolean getInsertedDie();
 
     void setInsertedDie(boolean b);
 
+    /**
+     * This method allows client to notify server the use of tool card 8
+     * @param isTool8Used true if toolCard 8 is used
+     */
     void setTool8Used(boolean isTool8Used);
 
-    //Stabilire il turno in cui ci si trova
+    /**
+     * This method allows a player to understand if the turn he is playing is the first, or the second, of the round
+     * @return true if turn's "way" is clockwise
+     */
     boolean getClockwiseRound();
 
-    //Indica quanti token rimangono al giocatore
+    /**
+     *
+     * @param name player's nickname
+     * @return remaining player's tokens
+     */
     int getTokenRemaining(String name);
 
-    //Restituisce un valore true se sono l'unico giocatore attivo al momento dello skip
+    /**
+     *
+     * @return true if player who calls this method is the only one active in game
+     */
     boolean iAmAlone();
 
+    /**
+     * ???
+     * @return
+     */
     int getStartTimeMove();
 
+    /**
+     * This method allows player to set his nickname
+     * @param userName player's nickname
+     */
     void setName(String userName);
 
-    //Da usare solo lato RMI
+    /**
+     * This method handles connection errors RMI side
+     */
     void handleConnectionError();
 
+    /**
+     * This method allows player to stop the research of other players
+     * @param name ???
+     * @return ???
+     */
     //Per annullare la ricerca del giocatore
     boolean removePlayer(String name);
 
+    /**
+     * Method to handle player's reconnection
+     * @return ???
+     */
     //Per notificare se un giocatore è uscito o rientrato
     ArrayList<String> someoneLeftGame();
 
+    /**
+     * Method to handle player's reconnection
+     * @return  ???
+     */
     ArrayList<String> someoneRejoinedGame();
 
+    /**
+     * This method allows to stop player's turn timer
+     */
     void stopTimer();
 }
